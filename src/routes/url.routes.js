615bfed4374384cbc1a7086efe7d4aa9fcc62515
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-05-22 18:00:21 -0300

    fix: rota /urls/:id do método DELETE adicionada

--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/url.schema.js";
-import { urlsShorten, getUrlById, openShortUrl } from "../controllers/urls.controller.js";
+import { urlsShorten, getUrlById, openShortUrl, deleteUrl} from "../controllers/urls.controller.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten",validateSchema(urlSchema),urlsShorten);
 urlRouter.get("/urls/:id",getUrlById);
 urlRouter.get("/urls/open/:shortUrl",openShortUrl);
+urlRouter.delete("/urls/:id",deleteUrl);
 
 export default urlRouter;
 
